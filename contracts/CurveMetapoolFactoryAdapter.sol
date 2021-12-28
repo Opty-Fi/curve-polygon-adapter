652735dc4e2c9c89085289a59402b19bdf7a3d94
dhruvinparikh <imsilent93@gmail.com>
2021-12-27 20:48:23 -0500

    build(defi-legos): bump up defi legos

--- contracts/CurveMetapoolFactoryAdapter.sol
+++ contracts/CurveMetapoolFactoryAdapter.sol
@@ -5,7 +5,13 @@ pragma solidity =0.8.11;
 import "./utils/AdapterInvestLimitBase.sol";
 // interfaces
 import { IAdapter } from "@optyfi/defi-legos/interfaces/defiAdapters/contracts/IAdapter.sol";
+import { ICurve2StableSwap } from "@optyfi/defi-legos/polygon/curve/contracts/ICurve2StableSwap.sol";
+import { ICurve3StableSwap } from "@optyfi/defi-legos/polygon/curve/contracts/ICurve3StableSwap.sol";
+import { ICurve4StableSwap } from "@optyfi/defi-legos/polygon/curve/contracts/ICurve4StableSwap.sol";
 
 contract CurveStableSwapAdapter is AdapterInvestLimitBase {
+    /**@notice address of metapool factory contract on matic */
+    address public constant CurveL2Factory = address(0x722272D36ef0Da72FF51c5A65Db7b870E2e8D4ee);
+
     constructor(address _registry) AdapterModifiersBase(_registry) {}
 }
