39e3dd00bd17dae70e0ce7731861cd1afc23432a
dhruvinparikh <imsilent93@gmail.com>
2022-01-10 10:28:43 -0500

    test(stableswap): test gest poolvalue and add skeleton

--- test/CurveStableSwapAdapter.behaviour.ts
+++ test/CurveStableSwapAdapter.behaviour.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { BigNumber } from "ethers";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 import { ICurve2StableSwap, ICurve3StableSwap } from "../typechain";
 import { PoolItem } from "./types";
 import { setTokenBalanceInStorage } from "./utils";
@@ -17,7 +17,7 @@ export function shouldBehaveLikeCurveStableSwapAdapter(token: string, pool: Pool
     // underlying token instance
     const underlyingTokenInstance = await hre.ethers.getContractAt("ERC20", pool.tokens[0]);
     // underlying token decimals
-    const underlyingTokenDecimals = await underlyingTokenInstance.decimals();
+    // const underlyingTokenDecimals = await underlyingTokenInstance.decimals();
     // stable swap's pool instance
     const stableSwap3Instance = <ICurve3StableSwap>await hre.ethers.getContractAt("ICurve3StableSwap", pool.pool);
     const stableSwap2Instance = <ICurve2StableSwap>await hre.ethers.getContractAt("ICurve2StableSwap", pool.pool);
@@ -26,7 +26,7 @@ export function shouldBehaveLikeCurveStableSwapAdapter(token: string, pool: Pool
     // lpToken instance
     const lpTokenInstance = await hre.ethers.getContractAt("ERC20", pool.lpToken);
     // lpToken decimals
-    const lpTokenDecimals = await lpTokenInstance.decimals();
+    // const lpTokenDecimals = await lpTokenInstance.decimals();
 
     // fund the testDefiAdapter with underlying tokens
     if (pool.pool == "0xC2d95EEF97Ec6C17551d45e77B590dc1F9117C67") {
@@ -38,7 +38,23 @@ export function shouldBehaveLikeCurveStableSwapAdapter(token: string, pool: Pool
     const balanceOfUnderlyingTokenInTestDefiAdapter = await underlyingTokenInstance.balanceOf(
       this.testDeFiAdapterForStableSwap.address,
     );
-    // 1. Deposit All underlying tokens
+    // 0.1 setWrappedTokens
+
+    // 0.2 setTokenIndexes
+
+    // 0.3 setNoZeroAllowanceAllowed
+
+    // 0.4 setCalcWithdrawOneCoinNotSame
+
+    // 1. lpToken
+    // 2. Underlying tokens
+    // 3. Pool value
+    const actualPoolValue = await this.curveStableSwapAdapter.getPoolValue(pool.pool, ethers.constants.AddressZero);
+    const _virtualPrice = await stableSwap2Instance.get_virtual_price();
+    const _totalSupply = await lpTokenInstance.totalSupply();
+    const expectedPoolValue = await _virtualPrice.mul(_totalSupply).div(BigNumber.from("10").pow("18"));
+    expect(actualPoolValue).to.eq(expectedPoolValue);
+    // 4. Deposit All underlying tokens
     let calculatedlpTokenAmount: BigNumber = BigNumber.from(0);
     if (nTokens[pool.pool] == "3" && pool.tokenIndexes[0] == "0") {
       calculatedlpTokenAmount = await stableSwap3Instance.calc_token_amount(
@@ -72,7 +88,7 @@ export function shouldBehaveLikeCurveStableSwapAdapter(token: string, pool: Pool
       this.curveStableSwapAdapter.address,
       balanceOfUnderlyingTokenInTestDefiAdapter,
     );
-    // 1.1 assert whether lptoken balance is as expected or not after deposit
+    // 5. assert whether lptoken balance is as expected or not after deposit
     const actuallpTokenBalance = await lpTokenInstance.balanceOf(this.testDeFiAdapterForStableSwap.address);
     expect(actuallpTokenBalance).to.gte(calculatedlpTokenAmount.mul(95).div(100));
     const expectedlpTokenBalance = await this.curveStableSwapAdapter.getLiquidityPoolTokenBalance(
@@ -81,6 +97,23 @@ export function shouldBehaveLikeCurveStableSwapAdapter(token: string, pool: Pool
       pool.pool,
     );
     expect(expectedlpTokenBalance).to.eq(actuallpTokenBalance);
+
+    // 6. all amount in token
+
+    // 7. some amount in token
+
+    // 8. calculate amount in lp token
+
+    // 9. calculateRedeemableLPTokenAmount
+
+    // 10. isRedeemableAmountSufficient
+
+    // 11. getRewardToken
+
+    // 12. canStake
+
+    // 13.
+
     // 6. Withdraw all lpToken balance
     const calculatedUnderlyingTokenBalanceAfterWithdraw = await stableSwap3Instance.calc_withdraw_one_coin(
       actuallpTokenBalance,
