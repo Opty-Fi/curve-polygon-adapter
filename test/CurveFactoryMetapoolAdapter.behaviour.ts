59907c596e188e35aada25c4528ee4689d3cc4ed
dhruvinparikh <imsilent93@gmail.com>
2022-01-19 00:48:57 -0500

    test(metapool): underlying token

--- test/CurveFactoryMetapoolAdapter.behaviour.ts
+++ test/CurveFactoryMetapoolAdapter.behaviour.ts
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { getAddress } from "ethers/lib/utils";
 import hre, { ethers } from "hardhat";
+import BN from "bignumber.js";
 import { ICurve2StableSwapMetapoolFactory } from "../typechain";
 import { ICurve3StableSwapMetapoolFactory } from "../typechain/ICurve3StableSwapMetapoolFactory";
 import { ICurve4StableSwapMetapoolFactory } from "../typechain/ICurve4StableSwapMetapoolFactory";
@@ -50,11 +51,12 @@ export function shouldBehaveLikeCurveFactoryMetapoolAdapter(token: string, pool:
         .connect(signer)
         .transfer(this.testDeFiAdapterForMetapoolFactory.address, "2000000000000000000000");
     } else {
-      if (
+      if (pool.tokens[0] == legos.tokens.LAMBO) {
+        await setTokenBalanceInStorage(underlyingTokenInstance, this.testDeFiAdapterForMetapoolFactory.address, "2");
+      } else if (
         pool.tokens[0] == legos.tokens.CRV ||
         pool.tokens[0] == legos.tokens.AMDAI ||
-        pool.tokens[0] == legos.tokens.RENUSDC ||
-        pool.tokens[0] == legos.tokens.LAMBO
+        pool.tokens[0] == legos.tokens.RENUSDC
       ) {
         await setTokenBalanceInStorage(underlyingTokenInstance, this.testDeFiAdapterForMetapoolFactory.address, "20");
       } else {
@@ -71,11 +73,9 @@ export function shouldBehaveLikeCurveFactoryMetapoolAdapter(token: string, pool:
       pool.lpToken,
     );
     // 2. Pool value
+    let actualPoolValue = BigNumber.from("0");
     try {
-      const actualPoolValue = await this.curveFactoryMetapoolAdapter.getPoolValue(
-        pool.pool,
-        ethers.constants.AddressZero,
-      );
+      actualPoolValue = await this.curveFactoryMetapoolAdapter.getPoolValue(pool.pool, ethers.constants.AddressZero);
       const _virtualPrice = await curve2StableSwapMetapoolFactoryInstance.get_virtual_price();
       const expectedPoolValue = _virtualPrice.mul(_totalSupply).div(BigNumber.from("10").pow("18"));
       expect(actualPoolValue).to.eq(expectedPoolValue);
@@ -85,66 +85,98 @@ export function shouldBehaveLikeCurveFactoryMetapoolAdapter(token: string, pool:
     }
 
     // ============================================
-    try {
-      let calculatedlpTokenAmount: BigNumber = BigNumber.from(0);
-      if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "0") {
-        calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [balanceOfUnderlyingTokenInTestDefiAdapter, 0, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "1") {
-        calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, balanceOfUnderlyingTokenInTestDefiAdapter, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "2") {
-        calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, 0, balanceOfUnderlyingTokenInTestDefiAdapter],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("2")) && pool.tokenIndexes[0] == "0") {
-        calculatedlpTokenAmount = await curve2StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [balanceOfUnderlyingTokenInTestDefiAdapter, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("2")) && pool.tokenIndexes[0] == "1") {
-        calculatedlpTokenAmount = await curve2StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, balanceOfUnderlyingTokenInTestDefiAdapter],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "0") {
-        calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [balanceOfUnderlyingTokenInTestDefiAdapter, 0, 0, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "1") {
-        calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, balanceOfUnderlyingTokenInTestDefiAdapter, 0, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "2") {
-        calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, 0, balanceOfUnderlyingTokenInTestDefiAdapter, 0],
-          true,
-        );
-      } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "3") {
-        calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
-          [0, 0, 0, balanceOfUnderlyingTokenInTestDefiAdapter],
-          true,
-        );
-      }
-      // ===========================================================
-      // 3. calculate amount in lp token
-      expect(
-        await this.curveFactoryMetapoolAdapter.calculateAmountInLPToken(
-          underlyingTokenInstance.address,
-          pool.pool,
-          balanceOfUnderlyingTokenInTestDefiAdapter,
-        ),
-      ).to.eq(calculatedlpTokenAmount);
-    } catch (error) {
-      console.error(`skipping ${token} as it throws error`);
-      this.skip();
+    let calculatedlpTokenAmount: BigNumber = BigNumber.from(0);
+    const _actualAmount: BigNumber = balanceOfUnderlyingTokenInTestDefiAdapter.mul(
+      BigNumber.from("10").pow(BigNumber.from(18).sub(underlyingTokenDecimals)),
+    );
+    const _limit = new BN(actualPoolValue.mul(BigNumber.from("10000")).toString()).div(new BN("10000"));
+    const _actualDepositAmount = _actualAmount.gt(BigNumber.from(_limit.toString()))
+      ? _limit.div(new BN("10").pow(new BN("18").minus(new BN(underlyingTokenDecimals))))
+      : balanceOfUnderlyingTokenInTestDefiAdapter;
+    if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "0") {
+      calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [_actualDepositAmount.toString(), 0, 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "1") {
+      calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, _actualDepositAmount.toString(), 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("3")) && pool.tokenIndexes[0] == "2") {
+      calculatedlpTokenAmount = await curve3StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, 0, _actualDepositAmount.toString()],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("2")) && pool.tokenIndexes[0] == "0") {
+      calculatedlpTokenAmount = await curve2StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [_actualDepositAmount.toString(), 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("2")) && pool.tokenIndexes[0] == "1") {
+      calculatedlpTokenAmount = await curve2StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, _actualDepositAmount.toString()],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "0") {
+      calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [_actualDepositAmount.toString(), 0, 0, 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "1") {
+      calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, _actualDepositAmount.toString(), 0, 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "2") {
+      calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, 0, _actualDepositAmount.toString(), 0],
+        true,
+      );
+    } else if (_nTokens.eq(BigNumber.from("4")) && pool.tokenIndexes[0] == "3") {
+      calculatedlpTokenAmount = await curve4StableSwapMetapoolFactoryInstance.calc_token_amount(
+        [0, 0, 0, _actualDepositAmount.toString()],
+        true,
+      );
     }
+    // ===========================================================
+    // 3. calculate amount in lp token
+    expect(
+      await this.curveFactoryMetapoolAdapter.calculateAmountInLPToken(
+        underlyingTokenInstance.address,
+        pool.pool,
+        _actualDepositAmount.toString(),
+      ),
+    ).to.eq(calculatedlpTokenAmount);
+    // 4. Deposit All underlying tokens
+    await this.testDeFiAdapterForMetapoolFactory.testGetDepositSomeCodes(
+      underlyingTokenInstance.address,
+      pool.pool,
+      this.curveFactoryMetapoolAdapter.address,
+      balanceOfUnderlyingTokenInTestDefiAdapter,
+    );
+    // 5. assert whether lptoken balance is as expected or not after deposit
+    const actuallpTokenBalance = await lpTokenInstance.balanceOf(this.testDeFiAdapterForMetapoolFactory.address);
+    expect(actuallpTokenBalance).to.gte(calculatedlpTokenAmount.mul(95).div(100));
+    const expectedlpTokenBalance = await this.curveFactoryMetapoolAdapter.getLiquidityPoolTokenBalance(
+      this.testDeFiAdapterForMetapoolFactory.address,
+      hre.ethers.constants.AddressZero,
+      pool.pool,
+    );
+    expect(expectedlpTokenBalance).to.eq(actuallpTokenBalance);
+
+    // assert remaining underlying token after deposit
+    const expectedBalanceOfUnderlyingTokenInTestDefiAdapterAfterDeposit = balanceOfUnderlyingTokenInTestDefiAdapter.sub(
+      _actualDepositAmount.toString(),
+    );
+    const balanceOfUnderlyingTokenInTestDefiAdapterAfterDeposit = await underlyingTokenInstance.balanceOf(
+      this.testDeFiAdapterForMetapoolFactory.address,
+    );
+    expect(expectedBalanceOfUnderlyingTokenInTestDefiAdapterAfterDeposit).to.closeTo(
+      balanceOfUnderlyingTokenInTestDefiAdapterAfterDeposit,
+      BigNumber.from("3")
+        .mul(BigNumber.from("10").pow(Math.ceil(underlyingTokenDecimals / 2)))
+        .toNumber(),
+    );
   });
 }
